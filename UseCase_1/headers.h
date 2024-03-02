9ff7ed28288c4abd146743b46ad9572d48722cbc
Paulo Fidalgo <paulofidalgo@PauloFidalgo.Home>
2024-03-02 19:08:29 +0000

    Defined macros for UC1

---
+++ UseCase_1/headers.h
@@ -0,0 +1,11 @@
+
+
+#define RECONF "reconfiguration"
+#define BEAM_CONF "beam_config"
+#define LIS_CONF "lis_config"
+#define GNB_RS "gNB_radio_sensing"
+#define UE_RS "ue_radio_sensing"
+#define LIS_RS "lis_radio_sensing"
+#define VIDEO_S "video_sensing"
+#define DATA "data"
+
