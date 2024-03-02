8c4fa30324d959dc4c6090e14eb5c96410553478
Paulo Fidalgo <paulofidalgo@PauloFidalgo.Home>
2024-03-02 19:10:10 +0000

    Defined macros for UC1

--- UseCase_1/headers.h
+++ UseCase_1/headers.h
@@ -1,5 +1,15 @@
+/*
+* File that defines headers for use case 1
+* Change IP and Port according to your needs
+*/
+#define MQTT_HOST "localhost"
+#define MQTT_PORT 1883
 
 
+/*
+* Topics to be used in Use Case 1
+* 
+*/
 #define RECONF "reconfiguration"
 #define BEAM_CONF "beam_config"
 #define LIS_CONF "lis_config"
